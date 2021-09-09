21d4628d628ac46d864901657d3359bcb02002e9
Mikhail Omelko <omelkom@gmail.com>
2021-09-09 17:43:41 +1000

    made base logic of the game

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // Incorrect input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not a Number!';
+
+    // Player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!!!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '20rem';
+    document.querySelector('.number').style.fontSize = '9rem';
+
+    // Guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        "💥 You've loose the game!";
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // Guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        "💥 You've loose the game!";
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
